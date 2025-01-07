4ec5c4559297a680b30cb7ec6da598f760cbdb30
MicheleSaraceni <michele.saraceni23@email.com>
2025-01-07 22:23:31 +0100

    connect with axios

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,13 +1,33 @@
-function Card({ items }) {
+// props = {
+// items: [{}, ...],
+// }
+
+// import { useState } from "react";
+
+// Possiamo destrutturare le props come segue:
+function Card({ item }) {
+    /* const [count, setCount] = useState(0);
+
+    function handleIncCount() {
+        setCount(prev => prev + 1);
+        setCount(prev => prev + 1);
+        setCount(prev => prev + 1);
+    }
+
+    function handleSubCount() {
+        setCount(prev => prev > 0 ? prev - 1 : prev);
+    } */
+
     return (
-        items.map((item) => (
-            <div className="card" key={item.id} style={{ width: "18rem" }}>
-                <div className="card-body">
-                    <h5 className="card-title">{item.titolo}</h5>
-                    <p className="card-text">{item.content}</p>
-                </div>
+        <div className="card" style={{ width: "18rem" }}>
+            <div className="card-body">
+                <h5 className="card-title">{item.titolo}</h5>
+                <p className="card-text">{item.content}</p>
+                {/* <button onClick={handleSubCount}>-</button>
+                <p>{count}</p>
+                <button onClick={handleIncCount}>+</button> */}
             </div>
-        ))
+        </div>
     )
 }
 
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,14 +1,35 @@
 import Form from "./Form";
 import Card from "./Card";
 import cardList from "../data/cardList";
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
 
 function Main() {
+
+    const [postList, setPostList] = useState([]);
+    const apiURL = "http://localhost:3000/posts";
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    function getData() {
+        axios
+            .get(apiURL)
+            .then((res) => {
+                console.log(res.data);
+                setPostList(res.data);
+            });
+    }
+
     return (
         <main className="d-flex flex-column justify-content-center align-items-center">
             <h1>Lista Card </h1>
             <Form />
             <div className="d-flex justify-content-center align-items-center flex-wrap">
-                <Card items={cardList} />
+                {/*cardList*/postList.map((post) => (
+                    <Card item={post} key={post.id} />
+                ))}
             </div>
         </main>
     )
