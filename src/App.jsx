4726667f1491ceca88ea2b31574cae61c6066464
MicheleSaraceni <michele.saraceni23@email.com>
2025-01-07 14:49:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Main from './component/main.jsx';
+import Header from './component/header.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
+
+export default App;
---
+++ src/component/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1>sono il Header </h1>
+        </header>
+    )
+}
+
+export default Header;
